5d1c59f5acfe1f4b580e841ce8d501805d968905
Rishitha <rishitha.m2022cse@sece.ac.in>
2026-02-16 07:00:02 +0530

    Save current changes before cleaning repo

--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,4 @@
-// import express from 'express';
-// import { register, login } from '../controllers/authController.js';
 
-// const router = express.Router();
-
-// router.post('/register', register);
-// router.post('/login', login);
-
-// export default router;
 
 import express from 'express';
 import { register, login } from '../controllers/authController.js';
